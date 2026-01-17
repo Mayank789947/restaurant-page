b5b7e8acdc68374708853ceee908a81e4714cd4e
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-01-17 11:11:17 +0530

    Added buttons functionality to load modules
    Added functionality on nav buttons to change modules on click

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import "./styles.css";
+import { homePage } from "./homepage/homepage.js";
+import { loadMenu } from "./menupage/menuPage.js";
+import { loadAbout } from "./about/about.js";
+import { loadContact } from "./contact/contact.js";
+
+
+const content = document.querySelector("#content");
+const buttons = document.querySelectorAll(".btn");
+
+function clearContent() {
+    content.innerHTML = "";
+    buttons.forEach(button => {
+        button.classList.remove("active");
+    })
+};
+
+document.querySelector(".home").addEventListener("click", () => {
+    clearContent();
+    homePage();
+});
+
+document.querySelector(".menu").addEventListener("click", () => {
+    clearContent();
+    loadMenu();
+});
+
+document.querySelector(".about").addEventListener("click", () => {
+    clearContent();
+    loadAbout();
+});
+
+document.querySelector(".contact").addEventListener("click", () => {
+    clearContent();
+    loadContact();
+});
+
+homePage();
